87ac34defe5be301b96610b8b8345f60ad283ce9
RANIE SOARES DA COSTA <54813595+ranie2019@users.noreply.github.com>
2023-10-26 20:53:19 -0300

    Listando jogadores que contam com mundos virtuais

--- VR Udemy Multiplayer/Assets/Scripts/RoomManager.cs
+++ VR Udemy Multiplayer/Assets/Scripts/RoomManager.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 
 public class RoomManager : MonoBehaviourPunCallbacks
 {
-    private string mapType; 
+    private string mapType;
+
+    public TextMeshProUGUI OccupancyRateText_forSchool;
+    public TextMeshProUGUI OccupancyRateText_forOutdoor;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -83,12 +88,43 @@ public class RoomManager : MonoBehaviourPunCallbacks
         Debug.Log(newPlayer.NickName + "Juntou-se a" + "Numero de Jogadores: " + PhotonNetwork.CurrentRoom.PlayerCount);
     }
 
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        if (roomList.Count == 0)
+        {
+            // não há espaço algum
+            OccupancyRateText_forSchool.text = 0 + " / " + 20;
+            OccupancyRateText_forOutdoor.text = 0 + " / " + 20;
+        }
+
+        foreach (RoomInfo room in roomList)
+        {
+            Debug.Log(room.Name);
+            if (room.Name.Contains(MultiplayerVRConstants.MAP_TYPE_VALUE_OUTDOOR))
+            {
+                //atualizar uma sala ao ar livre ocupação campo
+
+                Debug.Log("A Sala é um Mapa Ar Livre. A Contagem de Jogadores é: " + room.PlayerCount);
+                OccupancyRateText_forOutdoor.text = room.PlayerCount + " / " + 20;
+
+            }else if (room.Name.Contains(MultiplayerVRConstants.MAP_TYPE_VALUE_SCHOOL))
+            {
+                Debug.Log("A Sala é um Mapa Escolar. A Contagem de Jogadores é: " + room.PlayerCount);
+                OccupancyRateText_forSchool.text = room.PlayerCount + " / " + 20;
+            }
+        }
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Juntou-se ao Lobby");
+    }
     #endregion
 
     #region
     private void CreatedAndJoinRoom()
     {
-        string randomRoonName = "Sala_" + Random.Range(0, 10000);
+        string randomRoonName = "Sala_" + mapType + Random.Range(0, 10000);
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = 20;
 
