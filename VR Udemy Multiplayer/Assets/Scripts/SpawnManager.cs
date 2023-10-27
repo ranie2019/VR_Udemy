eac7196d9bbe50cea0f5a5d305e1876e05320815
RANIE SOARES DA COSTA <54813595+ranie2019@users.noreply.github.com>
2023-10-26 22:44:04 -0300

    Desovando jogadores de RV dentro da sala virtual

---
+++ VR Udemy Multiplayer/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject GenericVRPlayerPrefab;
+
+    public Vector3 spawnPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsConnectedAndReady)
+        {
+            PhotonNetwork.Instantiate(GenericVRPlayerPrefab.name, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
